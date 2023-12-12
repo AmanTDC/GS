beb7e9e558c305a9e724d488951b5a0fbaa1cb20
Suryansh <suryansh.shahi@squareboat.com>
2023-12-12 11:53:16 +0530

    FEAT: added services data

--- src/features/Services/sections/HeroSection.tsx
+++ src/features/Services/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = ({ data }: { data: any }) => {
     <div className='bg-[linear-gradient(97.87deg,#F0FDF4_0.87%,rgba(240,_253,_244,_0.2)_99.3%)] sm:p-12 px-5 py-12'>
       <div className='max-w-[1120px] mx-auto md:flex items-center gap-x-10 md:space-y-0 space-y-16 md:justify-between justify-center'>
         <div className='max-w-2xl space-y-6'>
-          <h1 className='md:text-5xl text-[40px] font-semibold text-black leading-[67px]'>
+          <h1 className='md:text-5xl text-[40px] font-semibold text-black !leading-[67px]'>
             {data?.title[0]}
             <span className='text-green-500'>{data?.title[1]}</span>
             {data?.title[2]}
